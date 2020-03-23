8ed4b26934829bcaebbaa21373740064915b2746
Justin Lupica <justinlupica@gmail.com>
2020-03-23 11:43:52 -0400

    3/23/2020

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const customerRouter = require("./routes/customers");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(express.static("scripts"));
 app.use(bodyParser.urlencoded({ entended: false }));
 
