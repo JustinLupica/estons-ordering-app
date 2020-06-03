25a4bc0cf3cb34fbc75f98b1b70af0082f57dbfe
JustinLupica <60518590+JustinLupica@users.noreply.github.com>
2020-06-02 21:03:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -10,21 +10,6 @@ function main() {
     height: 1000
   });
   mainWindow.loadURL(`http://localhost:8080/`);
-  mainWindow.once("ready-to-show", () => {
-    autoUpdater.checkForUpdatesAndNotify();
-  });
-}
 
-autoUpdater.on("update-available", () => {
-  mainWindow.webContents.send("update_available");
-});
-
-autoUpdater.on("update-downloaded", () => {
-  mainWindow.webContents.send("update_downloaded");
-});
-
-ipcMain.on("restart-app", () => {
-  autoUpdater.quitAndInstall();
-});
 
 app.on("ready", main);
